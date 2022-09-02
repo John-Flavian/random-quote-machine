f151865eb48d6f6f07f560b0317d7ba5a7819633
John Flavian <flavianomeokwe@gmail.com>
2022-09-02 14:45:36 +0100

    final version with footer.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import  Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core'
+
+var bg = 'linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.2) 25%, rgba(255, 255, 255, 0.2) 75%, rgba(255, 255, 255, 0) 100%)';
+const myStyles = makeStyles({
+    container: {
+      display: 'grid',
+      bottom: '0',
+      textAlign: 'center',
+      backgroundColor: '#99b3ff',
+      background: bg,
+    }
+})
 
 function Footer() {
+    const classes = myStyles();
     return(
-        <Grid
-  container
-  direction="row"
-  justify="flex-end"
-  alignItems="flex-end"
->
-<Typography variant="subtitle1">
-    View this code on GitHub.
-</Typography>
-
-</Grid>
+        <Grid className={classes.container}
+        >
+            <Typography variant="p">
+               <b> &copy; <a href="https://github.com/John-Flavian"> John Flavian</a> 2020.</b>
+            </Typography>
+            <Typography variant="subtitle2">
+                View this code on <a href="https://github.com/John-Flavian/random-quote-machine"> GitHub.</a>
+            </Typography>
+        </Grid>
     )
 }
 
